9facca5dfe4dd1c94d4803e4570fe8dcdda9fecb
DanielFordHUB <105242930+DanielFordHUB@users.noreply.github.com>
2022-07-20 12:06:44 -0500

    progress on explore

--- wrangle.py
+++ wrangle.py
@@ -19,12 +19,12 @@ def get_new_zillow_data():
     and uses the get_db_url function to connect to said dataframe'''
     sql = '''
     SELECT 
-        bedroomcnt, 
-        bathroomcnt,
-        calculatedfinishedsquarefeet,
-        taxvaluedollarcnt,
-        yearbuilt,
-        taxamount,
+        bedroomcnt AS bedrooms, 
+        bathroomcnt AS bathrooms,
+        calculatedfinishedsquarefeet AS sq_ft,
+        taxvaluedollarcnt AS tax_value,
+        yearbuilt AS year_built,
+        taxamount AS tax_amnt,
         fips
     FROM
         properties_2017
@@ -45,20 +45,20 @@ def optimize_types(df):
     # Convert some columns to integers
     # fips, yearbuilt, and bedrooms can be integers
     df["fips"] = df["fips"].astype(int)
-    df["yearbuilt"] = df["yearbuilt"].astype(int)
-    df["bedroomcnt"] = df["bedroomcnt"].astype(int)    
-    df["taxvaluedollarcnt"] = df["taxvaluedollarcnt"].astype(int)
-    df["calculatedfinishedsquarefeet"] = df["calculatedfinishedsquarefeet"].astype(int)
+    df["year_built"] = df["year_built"].astype(int)
+    df["bedrooms"] = df["bedrooms"].astype(int)    
+    df["tax_value"] = df["tax_value"].astype(int)
+    df["sq_ft"] = df["sq_ft"].astype(int)
     return df
 
 
 def handle_outliers(df):
     """Manually handle outliers that do not represent properties likely for 99% of buyers and zillow visitors"""
-    df = df[df.bathroomcnt <= 6]
+    df = df[df.bathrooms <= 6]
     
-    df = df[df.bedroomcnt <= 6]
+    df = df[df.bedrooms <= 6]
 
-    df = df[df.taxvaluedollarcnt < 2_000_000]
+    df = df[df.tax_value < 2_000_000]
 
     return df
 
